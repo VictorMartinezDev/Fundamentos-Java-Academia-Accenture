7fdf074f7189c26a38b9ff132c12e4fc2ee38d04
Victor Martinez <vicmanuelg19@gmail.com>
2026-02-12 17:44:50 -0600

    Practica de herencia

---
+++ fundamentosJava/src/com/bootcamp/week1/herencia/Arrancable.java
@@ -0,0 +1,8 @@
+package com.bootcamp.week1.herencia;
+
+import java.util.ArrayList;
+
+interface Arrancable {
+    void arrancar();
+    void detener();
+}
---
+++ fundamentosJava/src/com/bootcamp/week1/herencia/Moto.java
@@ -0,0 +1,30 @@
+package com.bootcamp.week1.herencia;
+
+class Moto extends Vehiculo {
+    private int cilindrada;
+
+    public Moto(String marca, String modelo, int anio, int cilindrada) {
+        super(marca, modelo, anio);
+        this.cilindrada = cilindrada;
+    }
+
+    @Override
+    public String tipoVehiculo() { return "Moto"; }
+
+    @Override
+    public void arrancar() {
+        System.out.println("  La moto " + marca + " " + modelo
+                         + " ruge con " + cilindrada + "cc...");
+    }
+
+    @Override
+    public void detener() {
+        System.out.println("  La moto " + marca + " "
+                         + modelo + " se detiene.");
+    }
+
+    @Override
+    public String toString() {
+        return info() + " - " + cilindrada + "cc";
+    }
+}
